227a1f508429d216112bc595b9c28ba56279f609
fak1ch <slyvayva@mail.ru>
2023-06-19 08:30:11 +0300

    AS_develop: upload empty project and base folder hierarhy

---
+++ Assets/App/Scripts/General/SceneLoader/SceneLoader.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using App.Scripts.General.Singleton;
+using App.Scripts.Scenes.General;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace App.Scripts.General.LoadScene
+{
+    public class SceneLoader : MonoSingleton<SceneLoader>
+    {
+        public event Action OnSceneStartLoading;
+        public event Action OnSceneLoaded;
+        
+        [SerializeField] private float _timeUntilLoadScene;
+        [SerializeField] private SceneScriptableObject _sceneSO;
+        [SerializeField] private LoadingPanel _loadingPanel;
+
+        private void OnEnable()
+        {
+            SceneManager.sceneLoaded += SceneLoadedEvent;
+        }
+
+        private void OnDisable()
+        {
+            SceneManager.sceneLoaded -= SceneLoadedEvent;
+        }
+
+        private void SceneLoadedEvent(Scene arg0, LoadSceneMode arg1)
+        {
+            OnSceneLoaded?.Invoke();
+        }
+
+        public void LoadScene(SceneEnum sceneEnum)
+        {
+            OnSceneStartLoading?.Invoke();
+            _loadingPanel.StartLoading(_timeUntilLoadScene);
+            SceneManager.LoadScene(GetSceneNameByEnum(sceneEnum));
+        }
+
+        private string GetSceneNameByEnum(SceneEnum sceneEnum)
+        {
+            return _sceneSO.scenes.FirstOrDefault(scene => scene.sceneEnumEnum == sceneEnum)?.sceneName;
+        }
+    }
+}
---
+++ Assets/App/Scripts/Installers/LevelSceneInstaller.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace App.Scripts.Installers
+{
+    public class LevelSceneInstaller : Installer
+    {
+        private void Awake()
+        {
+            
+        }
+    }
+}
